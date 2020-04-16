f2e6ddcaf53bd12ff371c948e6fdf39937781ec7
Tom <tomhbasmo@gmail.com>
2020-04-16 23:05:47 +0200

    La til combobox for nylige filer

--- src/main/java/org/programutvikling/gui/FileHandling.java
+++ src/main/java/org/programutvikling/gui/FileHandling.java
@@ -48,6 +48,8 @@ public class FileHandling {
             FileSaver saver = null;
             saver = getFileSaver(directoryPath.toString());
             //Dialog.showErrorDialog("Du kan bare lagre til enten txt eller jobj filer.");
+
+            //todo hvorfor er denne nullpointer?
           /**  ContextModel.INSTANCE.getSavedPathRegister().getListOfSavedFilePaths().add(directoryPath.toString());*/
             // ContextModel.INSTANCE.getSavedPathRegister().addPathToListOfSavedFilePaths(directoryPath.toString());
             try {
