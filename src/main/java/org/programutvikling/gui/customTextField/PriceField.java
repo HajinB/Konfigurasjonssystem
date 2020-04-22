5c3ec847f09654de7e38a50984b856038fd7771c
Ola Gynnild Berg <s341874@oslomet.no>
2020-04-22 15:25:44 +0200

    NameField, ZipField, MailField

---
+++ src/main/java/org/programutvikling/gui/customTextField/PriceField.java
@@ -0,0 +1,17 @@
+package org.programutvikling.gui.customTextField;
+
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+public class PriceField extends TextField {
+    private final String DOUBLE_REGEX = "^[0-9]+(.|,)?[0-9]?$";
+    public PriceField() {
+        super();
+
+        this.textProperty().addListener((ObservableValue<? extends String> observableValue, String oldValue, String newValue) -> {
+            if(!newValue.matches(DOUBLE_REGEX)) {
+                this.setText(oldValue);
+            }
+        });
+    }
+}
---
+++ src/main/java/org/programutvikling/gui/customTextField/ZipField.java
@@ -0,0 +1,17 @@
+package org.programutvikling.gui.customTextField;
+
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+public class ZipField extends TextField {
+    private final String ZIPNUMBER = "^[\\d]{4}+$";
+
+    public ZipField() {
+        super();
+        this.textProperty().addListener((ObservableValue<? extends String> observableValue, String oldValue, String newValue) -> {
+            if (!newValue.matches(ZIPNUMBER)) {
+                this.setText(oldValue);
+            }
+        });
+    }
+}
