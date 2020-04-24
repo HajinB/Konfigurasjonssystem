d9debd9516f3cb70c01cc4888d27b50cad4048c7
Hajin <s238727@oslomet.no>
2020-04-24 14:59:09 +0200

    Endret GUI-enduser og endret komponentnavn til engelsk i kodingen.

--- src/main/java/org/programutvikling/gui/EnduserController.java
+++ src/main/java/org/programutvikling/gui/EnduserController.java
@@ -33,16 +33,16 @@ public class EnduserController extends TabComponentsController {
     private Label lblTotalPrice;
 
     @FXML
-    private TableView<Component> tblProsessor;
+    private TableView<Component> tblProcessor;
 
     @FXML
-    private TableView<Component> tblSkjermkort;
+    private TableView<Component> tblVideo;
 
     @FXML
-    private TableView<Component> tblMinne;
+    private TableView<Component> tblMemory;
 
     @FXML
-    private TableView<Component> tblHarddisk;
+    private TableView<Component> tblHardDisc;
 
     @FXML
     private TableView<Component> tblSSD;
@@ -89,9 +89,9 @@ public class EnduserController extends TabComponentsController {
 
     private void updateComponentViews() {
         endUserService.updateEndUserRegisters();
-        ComponentRegister harddiskRegister = new ComponentRegister();
-        harddiskRegister.getRegister().addAll(endUserService.getHarddiskRegister().getRegister());
-        System.out.println(harddiskRegister.toString());
+        ComponentRegister hardDiscRegister = new ComponentRegister();
+        hardDiscRegister.getRegister().addAll(endUserService.getHarddiskRegister().getRegister());
+        System.out.println(hardDiscRegister.toString());
         //System.out.println(endUserService.getHarddiskRegister().toString());
         //endUserService.getHarddiskRegister().attachTableView(tblViewHarddisk);
         //tblViewHarddisk.setItems(endUserService.getHarddiskRegister().getObservableRegister());
@@ -111,34 +111,34 @@ public class EnduserController extends TabComponentsController {
 
     private void updateList() {
         updateTotalPrice();
-        setTblProsessor(tblProsessor);
-        setTblHarddisk(tblHarddisk);
-        setTblSkjermkort(tblSkjermkort);
-        setTblMinne(tblMinne);
+        setTblProcessor(tblProcessor);
+        setTblHardDisc(tblHardDisc);
+        setTblVideo(tblVideo);
+        setTblMemory(tblMemory);
 
     }
 
     /**
      * går via endUserService for å hente lister som er filtrert på produkttype
      */
-    private void setTblMinne(TableView<Component> tblMinne) {
-        tblMinne.setItems(endUserService.getMinneRegister().getObservableRegister());
-        this.tblMinne = tblMinne;
+    private void setTblMemory(TableView<Component> tblMemory) {
+        tblMemory.setItems(endUserService.getMinneRegister().getObservableRegister());
+        this.tblMemory = tblMemory;
     }
 
-    private void setTblSkjermkort(TableView<Component> tblSkjermkort) {
-        tblSkjermkort.setItems(endUserService.getSkjermkortRegister().getObservableRegister());
-        this.tblSkjermkort = tblSkjermkort;
+    private void setTblVideo(TableView<Component> tblVideo) {
+        tblVideo.setItems(endUserService.getSkjermkortRegister().getObservableRegister());
+        this.tblVideo = tblVideo;
     }
 
-    public void setTblProsessor(TableView<Component> tblProsessor) {
-        tblProsessor.setItems(endUserService.getProsessorRegister().getObservableRegister());
-        this.tblProsessor = tblProsessor;
+    public void setTblProcessor(TableView<Component> tblProcessor) {
+        tblProcessor.setItems(endUserService.getProsessorRegister().getObservableRegister());
+        this.tblProcessor = tblProcessor;
     }
 
-    public void setTblHarddisk(TableView<Component> tblHarddisk) {
-        tblHarddisk.setItems(endUserService.getHarddiskRegister().getObservableRegister());
-        this.tblHarddisk = tblHarddisk;
+    public void setTblHardDisc(TableView<Component> tblHardDisc) {
+        tblHardDisc.setItems(endUserService.getHarddiskRegister().getObservableRegister());
+        this.tblHardDisc = tblHardDisc;
 
     }
 
@@ -153,17 +153,17 @@ public class EnduserController extends TabComponentsController {
 
 
     @FXML
-    void btnAddProsessorToCart(ActionEvent event) {
-        // tblProsessor.getSelectionModel().getSelectedCells();
+    void btnBuyProcessor(ActionEvent event) {
+        // tblProcessor.getSelectionModel().getSelectedCells();
         /**all adding av componenter må skje via enduserservice - legg til en metode der som legger til*/
-        Component selectedComp = tblProsessor.getSelectionModel().getSelectedItem();
+        Component selectedComp = tblProcessor.getSelectionModel().getSelectedItem();
         ContextModel.INSTANCE.getComputer().addComponent(selectedComp);
         updateComputerListView();
     }
 
     @FXML
-    void btnAddSkjermkortToCart(ActionEvent event) {
-        Component selectedComp = tblSkjermkort.getSelectionModel().getSelectedItem();
+    void btnBuyVideo(ActionEvent event) {
+        Component selectedComp = tblVideo.getSelectionModel().getSelectedItem();
         /**all adding av componenter må skje via enduserservice(?) - legg til en metode der som legger til*/
         if (selectedComp != null) {
             ContextModel.INSTANCE.getComputer().addComponent(selectedComp);
@@ -182,5 +182,45 @@ public class EnduserController extends TabComponentsController {
         FileSaverTxt fileSaverTxt = new FileSaverTxt();
         String path = FileUtility.getFilePathFromSaveTXTDialog(stage);
         fileSaverTxt.save(ContextModel.INSTANCE.getComputer(), Paths.get(path));
+    }
+
+    public void btnBuyComputer(ActionEvent event) {
+
+    }
+
+    public void btnBuyScreen(ActionEvent event) {
+
+    }
+
+    public void btnBuyMemory(ActionEvent event) {
+
+    }
+
+    public void btnBuyHardDisc(ActionEvent event) {
+
+    }
+
+    public void btnBuyCabinet(ActionEvent event) {
+
+    }
+
+    public void btnBuyKeyBoard(ActionEvent event) {
+
+    }
+
+    public void btnBuyMouse(ActionEvent event) {
+
+    }
+
+    public void btnBuyOther(ActionEvent event) {
+
+    }
+
+    public void btnBuyMotherBoard(ActionEvent event) {
+
+    }
+
+    public void btnDeleteFromCart(ActionEvent event) {
+
     }
 }
