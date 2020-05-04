e738c08693a639132acd46451c68566e7e859a03
Ola Gynnild Berg <s341874@oslomet.no>
2020-05-04 19:33:23 +0200

    La til id på GridPane user

--- src/main/java/org/programutvikling/gui/RegistryUserLogic.java
+++ src/main/java/org/programutvikling/gui/RegistryUserLogic.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import org.programutvikling.domain.user.User;
 import org.programutvikling.gui.customTextField.PriceField;
+import org.programutvikling.gui.customTextField.ZipField;
 
 public class RegistryUserLogic {
     private GridPane gridPane;
@@ -15,23 +16,32 @@ public class RegistryUserLogic {
         this.gridPane = gridPane;
     }
 
-    User createUserFromGUIInputFields() {
-        try {
-            User user = createUser();
-        }
-    }
+//    User createUserFromGUIInputFields() {
+//        try {
+//            User user = createUser();
+//        }
+//    }
 
     private User createUser() {
-        boolean admin = getBoolean((CheckBox) gridPane.lookup("#UserAdmin"));
-        String username = getString((TextField) gridPane.lookup("#UserUsername"));
-        String password = getString((TextField) gridPane.lookup("#UserPassword"));
+        boolean admin = getBoolean((CheckBox) gridPane.lookup("#userAdmin"));
+        String username = getString((TextField) gridPane.lookup("#userUsername"));
+        String password = getString((TextField) gridPane.lookup("#userPassword"));
         String name = getString((TextField) gridPane.lookup("#userName"));
         String email = getString((TextField) gridPane.lookup("#userMail"));
         String address = getString((TextField) gridPane.lookup("#userAddress"));
-        String zip = getString((TextField) gridPane.lookup("#userName"));
-        String city = getString((TextField) gridPane.lookup("#userName"));
+        String zip = getString((ZipField) gridPane.lookup("#userZip"));
+        String city = getString((TextField) gridPane.lookup("#userCity"));
+
+        return new User(admin,username,password,name,email,address,zip,city);
     }
 
+    private boolean areInputFieldsEmpty() {
+        return ((TextField) gridPane.lookup("#userUsername")).getText().isEmpty() || ((TextField) gridPane.lookup("#userPassword")).getText().isEmpty()
+                || ((TextField) gridPane.lookup("#userName")).getText().isEmpty() || ((TextField) gridPane.lookup("#userMail")).getText().isEmpty()
+                || ((TextField) gridPane.lookup("#userAddress")).getText().isEmpty() || ((ZipField) gridPane.lookup("#userZip")).getText().isEmpty()
+                || ((TextField) gridPane.lookup("#userCity")).getText().isEmpty();
+
+    }
     // getters from gridPane
 
     private boolean getBoolean(CheckBox checkBox) {
@@ -41,4 +51,6 @@ public class RegistryUserLogic {
     private String getString(TextField textField) {
         return textField.getText();
     }
+
+
 }
