7573b4cddf531345af84ee391ec7a8cef1151710
Ola Gynnild Berg <s341874@oslomet.no>
2020-05-07 00:43:17 +0200

    NoSpacebarField i tabUsers.fxml

--- src/main/java/org/programutvikling/gui/customTextField/NoSpacebarField.java
+++ src/main/java/org/programutvikling/gui/customTextField/NoSpacebarField.java
@@ -3,10 +3,10 @@ package org.programutvikling.gui.customTextField;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TextField;
 
-public class UsernameField extends TextField {
+public class NoSpacebarField extends TextField {
     private static final String SPACEBAR_REGEX = "^[\\S.]*+$";
 
-    public UsernameField() {
+    public NoSpacebarField() {
         super();
 
         this.textProperty().addListener((ObservableValue<? extends String> observableValue, String oldValue, String newValue) -> {
