fff13c7c75f0a98b6a354458f91240214191b53f
Tom <tomhbasmo@gmail.com>
2020-05-12 19:24:28 +0200

    .

---
+++ src/Test/org/programutvikling/gui/utility/DialogTest.java
@@ -0,0 +1,38 @@
+package org.programutvikling.gui.utility;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DialogTest {
+
+    @Test
+    void showErrorDialog() {
+        Dialog.showErrorDialog("safhdhlkfdsalhkjafsdhkjfasdhkjlafsdhklkahlfskhjlfadskhljadfshkjlfsd " +
+                "safdhkkldfsahlhkjfsdahkjlfasdkhjlfadskhjlfhkadlhkjafldshklfjads");
+    }
+
+    @Test
+    void showInformationDialog() {
+    }
+
+    @Test
+    void showSuccessDialog() {
+    }
+
+    @Test
+    void getConfirmationAlert() {
+    }
+
+    @Test
+    void getOpenOption() {
+    }
+
+    @Test
+    void testGetConfirmationAlert() {
+    }
+
+    @Test
+    void getLoadingDialog() {
+    }
+}
